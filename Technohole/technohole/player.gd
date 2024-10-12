c77b607ee15cb8b7dba36af9a32cfa2e352d69a4
presto-manager <Tsaltsalkoa@mail.ru>
2024-10-12 16:33:32 +0300

    player model

---
+++ Technohole/technohole/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+
+const ACCELERATION = 500
+const MAX_SPEED = 60
+var motion = Vector2.ZERO
+
+var speed = 150
+var jumpForce = 500
+var gravity = 700
+var vel = Vector2()
+
+
+func _physics_process(delta: float) -> void:
+	
+
+
+	const SPEED = 300.0
+
+	var direction := Input.get_axis("ui_up", "ui_down")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+  # Get the input direction and handle the movement/deceleration.
+  # As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+#
+
+
+
+
+
+
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
