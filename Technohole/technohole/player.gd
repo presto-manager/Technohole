c01ff842e45f2ee447ea100bda5577ffbf9516b0
presto-manager <Tsaltsalkoa@mail.ru>
2024-10-13 13:41:20 +0300

    camera_move

--- Technohole/technohole/player.gd
+++ Technohole/technohole/player.gd
@@ -1,32 +1,68 @@
 extends CharacterBody2D
 
 @onready var wall_sprite = $main_room
+<<<<<<< Updated upstream
 const SPEED = 100.0
 var rotation_speed = 13.5
+=======
+var SPEED = 50.0
+var rotation_speed = 4
+var flag = 1
+>>>>>>> Stashed changes
 
 @export var line_scene: PackedScene
 @onready var ray_cast = $RayCast2D
 
 func _physics_process(delta: float) -> void:
+	print(rotation)
 	
+<<<<<<< Updated upstream
 	var direction := Input.get_axis("ui_up", "ui_down")
 	if direction:
 		velocity.y = direction * SPEED
+=======
+	if Input.is_action_pressed("ui_up"):
+		velocity.x = cos(rotation)
+		velocity.y = sin(rotation) 
+
+		sprite.play("run")	
+>>>>>>> Stashed changes
 	else:
+		velocity.x = move_toward(velocity.y, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+<<<<<<< Updated upstream
+=======
+
+		sprite.play("run")
+		
+	#if Input.is_action_pressed("ui_down"):
+		#rotation = lerp_angle(rotation, rotation-180, rotation_speed * delta)
+#
+		#await get_tree().create_timer(1).timeout
+		#sprite.play("run")
+
+		
+>>>>>>> Stashed changes
 	
   # Get the input direction and handle the movement/deceleration.
   # As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+<<<<<<< Updated upstream
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+=======
+		rotation += delta * direction
+		sprite.play("run")
+	#else:
+		#rotation = lerp_angle(rotation, 0, rotation_speed * delta)
+		#sprite.play("run")
+>>>>>>> Stashed changes
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
-		var target_rotation = velocity.angle()
-		rotation = lerp_angle(rotation, target_rotation, rotation_speed * delta)
+	
 	move_and_slide()
 
 func create_dot(point):
