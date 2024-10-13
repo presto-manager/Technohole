5ff251bfc84934001550e58f8eced3ca7466942b
Vlasom <vita1655373@gmail.com>
2024-10-13 07:51:01 +0300

    added time delete points

--- Technohole/technohole/player.gd
+++ Technohole/technohole/player.gd
@@ -9,6 +9,7 @@ var rotation_speed = 3.5
 
 
 var queue = [] # with len = 100
+var points = []
 const LEN_OF_QUEUE = 300
 func queue_add(elem, queue):
 	if elem in queue:
@@ -47,25 +48,39 @@ func _physics_process(delta: float) -> void:
 		var target_rotation = velocity.angle()
 		rotation = lerp_angle(rotation, target_rotation, rotation_speed * delta)
 	move_and_slide()
+	
+	
+	
+func create_dot(point, coeff, time):
 
-func create_dot(point, coeff):
+
 	var line_instance = line_scene.instantiate()
 	line_instance.position = Vector2(point)
 	coeff = coeff*8
 	line_instance.default_color = Color(255/coeff, 255/coeff, 255/coeff)
 	get_parent().add_child(line_instance)
 	queue_add(point,queue)
+	points.append(line_instance)
+	
+	
+	var timer = get_tree().create_timer(time)  # Создаем таймер на 5 секунд
+	await timer.timeout  # Ожидаем завершения таймера
+	line_instance.queue_free()  # Удаляем объект
 	
 
 func _ready() -> void:
 	ray_cast.visible = false
 
 func _process(delta):
+	
+	#var timer = get_tree().create_timer(5.0)
+		
 
 	if Input.is_action_just_pressed("ui_accept"):
 		ray_cast.visible = true
 		var player_position = self.position
 		var i = -60
+		var time = 5
 		while true:
 			
 			if i > 60:
@@ -76,10 +91,11 @@ func _process(delta):
 			var coeff = ((player_position[0]-point[0])**2+(player_position[1]-point[1])**2)**0.5
 			
 			if point:
-				create_dot(point, coeff)
+				create_dot(point, coeff, time)
 				print(point)
 				
 			await get_tree().create_timer(delta).timeout
 			i += 1
+			time += 1
 		ray_cast.rotation_degrees = 0
 		ray_cast.visible = false
