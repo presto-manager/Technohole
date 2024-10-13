63c72e9527fb58c6aa87fd2a9b5734212affb2d1
Vlasom <vita1655373@gmail.com>
2024-10-13 03:13:28 +0300

    added changing lidar points
    added changing lidar points lightness with distance of player

--- Technohole/technohole/player.gd
+++ Technohole/technohole/player.gd
@@ -29,23 +29,31 @@ func _physics_process(delta: float) -> void:
 		rotation = lerp_angle(rotation, target_rotation, rotation_speed * delta)
 	move_and_slide()
 
-func create_dot(point):
+func create_dot(point, coeff):
 	#var player_position = self.position
 	var line_instance = line_scene.instantiate()
 	line_instance.position = Vector2(point)
+	coeff = coeff*8
+	line_instance.default_color = Color(255/coeff, 255/coeff, 255/coeff)
+	
 	get_parent().add_child(line_instance)
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
+		var player_position = self.position
 		var i = -60
 		while true:
 			if i > 60:
 				break
 			ray_cast.rotation_degrees = i
 			var point = ray_cast.get_collision_point()
+			
+			var coeff = ((player_position[0]-point[0])**2+(player_position[1]-point[1])**2)**0.5
+			
 			if point:
-				create_dot(point)
+				create_dot(point, coeff)
 				print(point)
+				
 			await get_tree().create_timer(delta).timeout
 			i += 1
 		ray_cast.rotation_degrees = 0
